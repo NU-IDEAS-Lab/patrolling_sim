454df3d78bbf6fc9f95f3789ef4031f15d84241f
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-03-31 18:54:00 -0500

    Updated run_attrition script.

--- src/patrol_algorithms_cdc2023/run_attrition.bash
+++ src/patrol_algorithms_cdc2023/run_attrition.bash
@@ -11,15 +11,15 @@ ros2 doctor --report
 
 AGENTS=6
 RUNTIME=1800
-OUTPUT_FILE="$HOME/dev/patrolling_results/results_attrition.zarr"
-ATTRITION_TIMES="300.0,1300.0"
+OUTPUT_FILE="$HOME/dev/patrolling_results/results_attrition3.zarr"
+ATTRITION_TIMES='1150.0,'
+
 
 for i in {1..3}; do
 
-    ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_cdc2023 algorithm_name:=AHPA agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
-    ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_base algorithm_name:=CBLS agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
     ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_base algorithm_name:=DTAGreedy agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
     ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_base algorithm_name:=DTASSIPart agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
     ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_base algorithm_name:=GBS agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
-
+    ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_cdc2023 algorithm_name:=AHPA agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
+    ros2 launch patrolling_sim simulation.launch.py algorithm_pkg:=patrol_algorithms_base algorithm_name:=CBLS agent_count:=$AGENTS use_rviz:=false use_gzclient:=false runtime:=$RUNTIME output:="$OUTPUT_FILE" attrition_times:="$ATTRITION_TIMES"
 done
