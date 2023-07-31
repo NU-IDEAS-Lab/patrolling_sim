a4b65621c1788d0e559c92f735c2aceb95721813
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-07-31 17:15:04 -0500

    Now uses the new Grex launch file structure.

--- src/patrolling_sim/launch/simulation.launch.py
+++ src/patrolling_sim/launch/simulation.launch.py
@@ -11,10 +11,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
 from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
-import os
-from statistics import mean
-import configparser
 
 
 def generate_launch_description():
@@ -30,12 +26,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         # Arguments.
-        DeclareLaunchArgument(
-            'agent_count', default_value='1'
-        ),
-        DeclareLaunchArgument(
-            'use_rviz', default_value='false'
-        ),
         DeclareLaunchArgument(
             'map', default_value='cumberland'
         ),
@@ -69,23 +59,6 @@ def generate_launch_description():
             value="1"
         ),
 
-        # Launch the simulation base.
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('simulation_base'),
-                    'launch',
-                    'mas_simulation.launch.py'
-                ])
-            ]),
-            launch_arguments={
-                "use_agents": "false",
-                "use_rviz": LaunchConfiguration("use_rviz"),
-                "map": LaunchConfiguration("map"),
-                "gazebo_world_file": [FindPackageShare("simulation_base"), "/models/maps/", LaunchConfiguration("map"), "/model.sdf"]
-            }.items()
-        ),
-
         # Set parameters from file.
         SetParametersFromFile(LaunchConfiguration("params_file")),
 
@@ -108,10 +81,20 @@ def generate_launch_description():
             ]
         ),
 
-        # Agent nodes.
-        OpaqueFunction(
-            function=generate_agents,
-            args=[LaunchConfiguration('agent_count'), LaunchConfiguration('map')]
+        # Launch the simulation base.
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('grex'),
+                    'launch',
+                    'simulation.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                "map": LaunchConfiguration("map"),
+                "gazebo_world_file": [FindPackageShare("grex"), "/models/maps/", LaunchConfiguration("map"), "/model.sdf"],
+                "agent_launch_file": [FindPackageShare("patrolling_sim"), "/launch/agent.launch.py"],
+            }.items()
         ),
 
         # Monitor/control node.
@@ -123,96 +106,3 @@ def generate_launch_description():
             on_exit=EmitEvent(event=Shutdown(reason='monitor stopped')))
         )
     ])
-
-
-def generate_agents(context: LaunchContext, agent_count_subst, map_subst):
-    ''' Generates the list of agent launch descriptions. '''
-
-    # Convert contextual info to variables.
-    agent_count = int(context.perform_substitution(agent_count_subst))
-    map = str(context.perform_substitution(map_subst))
-
-    # Load starting positions.
-    try:
-        initPosesFile = os.path.join(get_package_share_directory("patrolling_sim"), "config", "initial_poses.txt")
-        initPosesDict = loadInitPoses(initPosesFile)
-        initPosesKey = map.lower() + "_" + str(agent_count)
-        initPoses = str(initPosesDict[initPosesKey])
-        initPoses = initPoses.split(" ")
-        initPosesFloat = [float(x) for x in initPoses]
-    except:
-        return [
-            LogInfo(
-                msg="Failed to create agents! Could not load initial poses from file {0} with key {1}".format(initPosesFile, initPosesKey)
-            ),
-            ShutdownAction(
-                reason="Failed to create agents! Could not load initial poses from file {0} with key {1}".format(initPosesFile, initPosesKey)
-            )
-        ]
-
-    agents = []
-    for agent in range(agent_count):
-        agents += [
-            LogInfo(msg=TextSubstitution(text="Creating patrol agent " + str(agent))),
-            GroupAction(
-                actions = [
-                    # Set parameters.
-                    SetParameter(name="id_robot", value=str(agent)),
-                    SetParameter(name="initial_pos.x", value=str(initPoses[agent * 2])),
-                    SetParameter(name="initial_pos.y", value=str(initPoses[agent * 2 + 1])),
-                    SetParameter(name="initial_poses", value=str(initPosesFloat)),
-
-                    # Include the robot launch file.
-                    IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource([
-                            PathJoinSubstitution([
-                                FindPackageShare('patrolling_sim'),
-                                'launch',
-                                'robot.launch.py'
-                            ])
-                        ]),
-                        launch_arguments={
-                            "id": str(agent),
-                            "name": "agent" + str(agent),
-                            "algorithm_pkg": LaunchConfiguration("algorithm_pkg"),
-                            "algorithm_name": LaunchConfiguration("algorithm_name"),
-                        }.items()
-                    ),
-
-                    # Launch the actual robot (copied from simulation_base)
-                    IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource([
-                            PathJoinSubstitution([
-                                FindPackageShare('simulation_base'),
-                                'launch',
-                                'robot.launch.py'
-                            ])
-                        ]),
-                        launch_arguments={
-                            "id": str(agent),
-                            "name": "agent" + str(agent),
-                            "use_rviz": LaunchConfiguration("use_rviz"),
-                            "map": LaunchConfiguration("map"),
-                            "pose_x": str(initPoses[agent * 2]),
-                            "pose_y": str(initPoses[agent * 2 + 1]),
-                        }.items()
-                    )
-                ]
-            )
-        ]
-    
-    return agents
-
-
-def loadInitPoses(configFile):
-    ''' Load initial poses from configuration file. '''
-    initPoses = {}
-    try:
-        ConfigIP = configparser.ConfigParser()
-        ConfigIP.read(configFile)
-        for option in ConfigIP.options("InitialPoses"):
-            # print(option)
-            initPoses[option] = ConfigIP.get("InitialPoses", option)
-    except:
-        print("Could not load initial poses file")
-    return initPoses
