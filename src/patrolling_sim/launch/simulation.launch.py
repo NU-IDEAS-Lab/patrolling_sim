f455f464d53e61911a154da27fbbc43b3f1430c4
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-12-06 12:48:16 -0600

    Fixed launch config.

--- src/patrolling_sim/launch/simulation.launch.py
+++ src/patrolling_sim/launch/simulation.launch.py
@@ -92,6 +92,7 @@ def generate_launch_description():
             ]),
             launch_arguments={
                 "map": LaunchConfiguration("map"),
+                "map_file": [FindPackageShare("patrolling_sim"), "/models/maps/", LaunchConfiguration("map"), "/", LaunchConfiguration("map"), ".yaml"],
                 "flatland_world_file": [FindPackageShare("patrolling_sim"), "/models/maps/", LaunchConfiguration("map"), "/", LaunchConfiguration("map"), "_flatland.yaml"],
                 "gazebo_world_file": [FindPackageShare("patrolling_sim"), "/models/maps/", LaunchConfiguration("map"), "/model.sdf"],
                 "agent_launch_file": [FindPackageShare("patrolling_sim"), "/launch/agent.launch.py"],
