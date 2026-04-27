157feb2c144f7ff08c20c7468e28de1b4e965c4c
Compsci-RiceTia <tiarice2028@u.northwestern.edu>
2026-04-27 17:21:36 -0500

    add aprilTags to PzAgent

--- src/patrol_algorithms_magec/patrol_algorithms_magec/PzAgent.py
+++ src/patrol_algorithms_magec/patrol_algorithms_magec/PzAgent.py
@@ -4,6 +4,9 @@ from std_msgs.msg import Int16MultiArray
 
 from patrol_algorithms_ahpa.BasePatrolAgent import BasePatrolAgent
 from patrolling_sim_interfaces.srv import QueryPolicy
+from apriltag_msgs.msg import AprilTagDetectionArray
+from patrolling_sim_interfaces.srv import Coordinates
+from tf2_ros import TransformException
 
 
 class PzAgent(BasePatrolAgent):
@@ -21,6 +24,21 @@ class PzAgent(BasePatrolAgent):
         
         self.get_logger().info(f"PZ agent {self.id} initialization complete.")
         self.pzReady = True
+
+        self.subscription = self.create_subscription(
+            AprilTagDetectionArray,
+            'detections',
+            self.listener_callback,
+            10
+        )
+
+        self.observation = {}
+
+        self.srv = self.create_service(
+            Coordinates,
+            'GetCoordinates',
+            self.apriltag_callback
+        )
 
     def onExperimentInitialized(self):
         ''' Begins execution after experiment initialization completes. '''
@@ -66,7 +84,40 @@ class PzAgent(BasePatrolAgent):
             nextNode = None
 
         self.goToNode(nextNode)
+        
+    def apriltag_callback(self,request,response):
+        if request.id in self.observation.keys():
+            response.x,response.y,response.z=self.observation[request.id]
+            response.success=True
+            self.get_logger().info(f"x: {response.x}, y: {response.y}, z: {response.z},ID: {request.id}")
+            return response
+        else:
+             self.get_logger().info(f"the robot hasn't seen this apriltag,response is {None}")
+             return None
 
+
+    def listener_callback(self, msg):  
+        
+        for detection in msg.detections:
+
+            from_frame_rel ='map'
+            to_frame_rel = detection.family+":"+str(detection.id)
+            try:
+                t = self.tf_buffer.lookup_transform(
+                            to_frame_rel,
+                            from_frame_rel,
+                            rclpy.time.Time())
+                self.observation[to_frame_rel]=(t.transform.translation.x,t.transform.translation.y,t.transform.translation.z)
+                self.get_logger().info(f"position: {self.observation}")
+                self.get_logger().info(
+                f'Successfully transformed {to_frame_rel} to {from_frame_rel}')
+                self.get_logger().info(
+                f"Translation x: {t.transform.translation.x}, y: {t.transform.translation.y}, z: {t.transform.translation.z}, ID: {to_frame_rel}")
+
+            except TransformException as ex:
+                        self.get_logger().info(
+                            f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
+                        return
 
 def main(args=None):
     # Initialize ROS2.
